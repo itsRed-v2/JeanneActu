aad75d95c15c964a3b59430e26eb807deb8346fc
itsRed-v2 <paul.charignon.v2@gmail.com>
2023-12-04 21:26:56 +0100

    Static hosting

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello world!');
+	res.send("Hey ! Maybe try to see what's at /index.html ...");
 });
+
+app.use(express.static('files'));
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
