78dda122b787abf22e6574a83c314b29409656bc
itsRed-v2 <paul.charignon.v2@gmail.com>
2023-12-06 16:33:02 +0000

    Début de site web

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 	res.send("Hey ! Maybe try to see what's at /index.html ...");
 });
 
-app.use(express.static(path.join(__dirname, 'files')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
