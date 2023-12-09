75ed5dd5b62bc146faa6fb65ead7fdcf893b1b88
itsRed-v2 <paul.charignon.v2@gmail.com>
2023-12-09 14:37:17 +0000

    Mise en place de traefik

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
 	res.send("Hey ! Maybe try to see what's at /index.html ...");
