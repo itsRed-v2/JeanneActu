0a5a67186ad5a6ea8f478ffaec04c14b717df7a1
itsRed-v2 <paul.charignon.v2@gmail.com>
2024-01-14 18:04:22 +0000

    Ajout de la connection à la db

---
+++ src/db.js
@@ -0,0 +1,73 @@
+import mysql from 'mysql';
+
+class DB {
+	constructor(config) {
+		this.config = config;
+		this.connected = false;
+		this.connectAttempts(10);
+	}
+
+	async connectAttempts(maxAttempts) {
+		return new Promise((resolve, reject) => {
+			let attempts = 0;
+
+			const id = setInterval(() => {
+				this.connect()
+					.then((res) => {
+						clearInterval(id);
+						this.connected = true
+						resolve(res);
+					})
+					.catch((reason) => {
+						attempts++;
+						if (attempts >= maxAttempts) {
+							clearInterval(id);
+							console.log(`Error: failed to connect to database after ${attempts} attempts.`);
+							reject(reason);
+						} else {
+							console.log(`Failed to connect to database (attempt ${attempts}). Retrying in 1s.`);
+						}
+					});
+			}, 1000);
+		});
+	}
+
+	async connect() {
+		return new Promise((resolve, reject) => {
+			this.connection = mysql.createConnection(this.config);
+			this.connection.connect(err => {
+				if (err) reject(err);
+				else {
+					this.connection.on('error', err => {
+						this.connected = false;
+						if (err.code === "PROTOCOL_CONNECTION_LOST") {
+							console.log("Database connection lost, reconnecting...");
+							this.connectAttempts(10);
+						} else {
+							throw err;
+						}
+					});
+					console.log("Database connected.");
+					resolve();
+				}
+			});
+		});
+	}
+
+	async query(sql) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
+	}
+
+	async end(callback) {
+		this.connection.end(callback);
+	}
+}
+
+export {
+	DB
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import url from 'url';
+import { readFileSync } from 'fs';
+
+import { DB } from './db.js';
+
+const config = JSON.parse(readFileSync(new URL('config.json', import.meta.url)));
+
+const db = new DB(config.DB);
 
 const app = express();
 const port = 3000;
 
+// Express thingies
 app.get('/', (req, res) => {
 	res.redirect('/home');
 });
@@ -18,6 +26,16 @@ app.get('/lepatron', (req, res) => {
 
 app.use(express.static(resourcePath('public')));
 
+app.get('/db', (req, res) => {
+	if (!db.connected) {
+		res.status(503);
+		res.send("503 Service Unavailable");
+		return;
+	}
+
+	db.query("SELECT * FROM Video").then(result => res.send(result));
+});
+
 const server = app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
@@ -27,6 +45,9 @@ process.on('SIGTERM', () => {
 	console.log('SIGTERM signal received: closing HTTP server');
 	server.close(() => {
 		console.log("HTTP server closed");
+	});
+	db.end(err => {
+		console.log("Database connection closed");
 	});
 });
 
