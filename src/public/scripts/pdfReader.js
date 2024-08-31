56eed5febf0b86e5b3fa10834dd74f693b3afe35
itsRed-v2 <paul.charignon.v2@gmail.com>
2024-08-31 19:06:28 +0000

    Viewer avec résolution et scaling dynamique

--- src/public/scripts/pdfReader.js
+++ src/public/scripts/pdfReader.js
@@ -1,74 +1,129 @@
-// #####################
-// ### PDF RENDERING ###
-// #####################
-
+// Initialization of pdfjsLib
 import "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs";
 const pdfjsLib = globalThis.pdfjsLib;
 pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs";
 
-const pdfUrl = globalThis.pdfUrl;
+// Constant declaration 
+const PDF_URL = globalThis.pdfUrl; // This value is passed through by EJS with a script tag
 
-console.log("Loading:", pdfUrl);
-pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
+// Global variables
+let pdf;
+let viewerState;
+let zoom = 100; // Zoom is in percentage (to prevent floating-point errors on repeated additions / subractions)
+
+document.addEventListener("DOMContentLoaded", async event => {
+    // PDF and state machine initialisation
+    console.log("Loading:", PDF_URL);
+    pdf = await pdfjsLib.getDocument(PDF_URL).promise
     console.log("Pdf has loaded.");
 
+    viewerState = {
+        leftPageNumber: 1,
+        rightPageNumber: 2,
+        showRightPage: true,
+    };
+
+    // Initial canvas draw.
+    draw();
+
+    // Event registering
+    window.addEventListener('resize', () => {
+        resizeAllCanvases();
+        scheduleDraw();
+    });
+
+    document.getElementById("zoom-in-button").addEventListener('click', onZoomIn);
+    document.getElementById("zoom-out-button").addEventListener('click', onZoomOut);
+    document.getElementById("zoom-reset-button").addEventListener('click', onZoomReset);
+});
+
+// ### PDF RENDERING / CANVAS MANIPULATION ###
+
+function resizeAllCanvases() {
+    updateCanvasSize(pdf, viewerState.leftPageNumber, 'left-page');
+    updateCanvasSize(pdf, viewerState.rightPageNumber, 'right-page');
+}
+
+async function updateCanvasSize(pdf, pageNumber, canvasId) {
+    let page = await pdf.getPage(pageNumber)
+    let viewport = getScaledViewport(page);
+    let canvas = document.getElementById(canvasId);
+
+    canvas.style.width = Math.floor(viewport.width) + "px";
+    canvas.style.height =  Math.floor(viewport.height) + "px";
+}
+
+let drawScheduleId = undefined;
+
+function scheduleDraw() {
+    if (drawScheduleId) 
+        clearTimeout(drawScheduleId);
+
+    drawScheduleId = setTimeout(() => {
+        drawScheduleId = undefined;
+        draw();
+    }, 500);
+}
+
+function draw() {
+    drawPage(pdf, viewerState.leftPageNumber, 'left-page');
+    drawPage(pdf, viewerState.rightPageNumber, 'right-page');
+}
+
+async function drawPage(pdf, pageNumber, canvasId) {
+    updateCanvasSize(pdf, pageNumber, canvasId);
+
+    // Support HiDPI-screens.
+    let resolutionFactor = window.devicePixelRatio || 1;
+
+    let page = await pdf.getPage(pageNumber);
+    let viewport = getScaledViewport(page);
+    let canvas = document.getElementById(canvasId);
+
+    canvas.width = Math.floor(viewport.width * resolutionFactor);
+    canvas.height = Math.floor(viewport.height * resolutionFactor);
+
+    let context = canvas.getContext('2d');
+    let transform = resolutionFactor !== 1 ? [resolutionFactor, 0, 0, resolutionFactor, 0, 0] : null;
+
+    let renderContext = {
+        canvasContext: context,
+        transform: transform,
+        viewport: viewport
+    };
+    page.render(renderContext);
+}
+
+function getScaledViewport(page) {
+    const maxSize = getCanvasMaxSize();
+    let viewport = page.getViewport({ scale: 1, });
+    let scaleX = maxSize.width / viewport.width;
+    let scaleY = maxSize.height / viewport.height;
+    let minScale = Math.min(scaleX, scaleY);
+    let scaledViewport = page.getViewport({ scale: minScale * (zoom / 100) });
+    return scaledViewport;
+}
+
+function getCanvasMaxSize() {
     const view = document.getElementById("view-area");
+    
     // "- 40" is for the margins (20px on both sides)
     // "- 5" is for the small space between the two pages
     // "/ 2" is because there are two pages: we split their available space in two.
     const maxWidth = (view.clientWidth - 40 - 5) / 2;
     const maxHeight = view.clientHeight - 40;
 
-    renderPage(pdf, 1, 'left-page', maxWidth, maxHeight);
-    renderPage(pdf, 2, 'right-page', maxWidth, maxHeight);
-});
-
-function renderPage(pdf, pageNumber, canvasId, maxWidth, maxHeight) {
-    pdf.getPage(pageNumber).then(page => {
-
-        let viewport = page.getViewport({ scale: 1, });
-        let scaleX = maxWidth / viewport.width;
-        let scaleY = maxHeight / viewport.height;
-        let effectiveScale = Math.min(scaleX, scaleY);
-        let scaledViewport = page.getViewport({ scale: effectiveScale });
-        
-        // Support HiDPI-screens.
-        let resolutionFactor = window.devicePixelRatio || 1;
-
-        let canvas = document.getElementById(canvasId);
-        let context = canvas.getContext('2d');
-
-        canvas.width = Math.floor(scaledViewport.width * resolutionFactor);
-        canvas.height = Math.floor(scaledViewport.height * resolutionFactor);
-        canvas.style.width = Math.floor(scaledViewport.width) + "px";
-        canvas.style.height =  Math.floor(scaledViewport.height) + "px";
-
-        let transform = resolutionFactor !== 1 ? [resolutionFactor, 0, 0, resolutionFactor, 0, 0] : null;
-
-        let renderContext = {
-            canvasContext: context,
-            transform: transform,
-            viewport: scaledViewport
-        };
-        page.render(renderContext);
-    });
+    return {
+        width: maxWidth, 
+        height: maxHeight 
+    };
 }
 
-// #####################
 // ### ZOOM CONTROLS ###
-// #####################
-
-document.addEventListener("DOMContentLoaded", event => {
-    document.getElementById("zoom-in-button").addEventListener('click', onZoomIn);
-    document.getElementById("zoom-out-button").addEventListener('click', onZoomOut);
-    document.getElementById("zoom-reset-button").addEventListener('click', onZoomReset);
-});
-
-let zoom = 100; // Zoom is in percentage (to prevent floating-point errors on repeated additions / subractions)
 
 function onZoomIn() {
     if (zoom >= 300) return;
-
+  
     if (zoom >= 160) zoom += 20;
     else zoom += 10;
 
@@ -90,6 +145,7 @@ function onZoomReset () {
 }
 
 function updateZoom() {
-    document.getElementById("page-container").style.transform = `scale(${ zoom / 100 })`;
-    document.getElementById("toolbar__zoom-value").textContent = Math.round(zoom) + "%";
+    resizeAllCanvases();
+    scheduleDraw();
+    document.getElementById("toolbar__zoom-value").textContent = zoom + "%";
 }
